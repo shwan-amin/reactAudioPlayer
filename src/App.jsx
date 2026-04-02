32ebddc8edfddc22ca50cf67f3f770ced1e7d2fa
Shwan Amin <z5684089@ad.unsw.edu.au>
2026-04-02 17:12:50 +1100

    Finished set up of App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
 import './App.css'
 import React from "react"
+import Card from './components/ui/Card.jsx'
+import Button from "./components/ui/Button.jsx"
+import NowPlaying from "./components/NowPlaying.jsx"
+import Player from "./components/player/Player.jsx"
+import Controls from "./components/player/Controls.jsx"
+import SeekBar from "./components/player/SeekBar.jsx"
+import Queue from "./components/queue/Queue.jsx"
 
+const PlayerContext = React.createContext();
 function App() {
-  const PlayerContext = React.createContext();
+  // Init the context that will be accessed from the whole App
+
+  // Set Default States
+  const [queue, setQueue] = React.useState([
+    {
+      id: 1,
+      title: "Stove Steak",
+      src: "../Gagmesharkoff - Stove Steak.mp3"
+    },
+    {
+      id: 2,
+      title: "Enchanted walley whistle",
+      src: "../Ondrosik - Enchanted walley whistle extended version.mp3"
+    },
+    {
+      id: 3,
+      title: "In A Twinkling of An Eye",
+      src: "../Pamela Yuen - In A Twinkling Of An Eye.mp3"
+    }
+  ]);
+
+  const [currentTrack, setCurrentTrack] = React.useState(null);
+  const [isPlaying, setIsPlaying] = React.useState(false);
+  const [volume, setVolume] = React.useState(0.8);
+  const audioRef = React.useRef(null);
 
   return (
     <PlayerContext.Provider value={{
@@ -14,15 +46,29 @@ function App() {
       audioRef,
 
       // Functions
+      setQueue,
       setCurrentTrack,
       setIsPlaying,
-      setVolume,
-      setQueue
+      setVolume
     }}>
+      <audio ref={audioRef} />
+
+      <Card>
+        <NowPlaying />
+      </Card>
+
+      <Card>
+        <Player>
+          <Controls />
+          <SeekBar />
+        </Player>
+      </Card>
+
+      <Card>
+        <Queue />
+      </Card>
     </PlayerContext.Provider>
   );
-
-  return <></>
 }
 
 export default App
---
+++ src/components/NowPlaying.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function NowPlaying() {
+    return (
+        <h1>Yes</h1>
+    );
+}
